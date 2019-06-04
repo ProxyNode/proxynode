65310cf33d06d65fc4acd0a0ae9450c5d618e3d0
ProxyNode <44655174+ProxyNode@users.noreply.github.com>
2019-06-04 23:45:10 +0100

    Path update for new release
    https://github.com/ProxyNode/proxynode/releases/download/v2.0.0/Proxynode-Linux-MN-v2.0.0.zip

--- proxynode-mn-install.sh
+++ proxynode-mn-install.sh
@@ -38,7 +38,7 @@ then
   sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
   cd
 
-  wget https://github.com/proxynode/proxynode/releases/download/v1.0.0/Linux.zip
+  wget https://github.com/ProxyNode/proxynode/releases/download/v2.0.0/Proxynode-Linux-MN-v2.0.0.zip
   unzip Linux.zip
   chmod +x Linux/bin/*
   sudo mv  Linux/bin/* /usr/local/bin
